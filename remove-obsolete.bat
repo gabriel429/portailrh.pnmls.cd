33460b1d01aaeba310601723b601f7bd0b37c901
serveurpnmls-cpu <serveurpnmls@gmail.com>
2026-04-09 13:23:32 +0100

    chore: purge arborescence - 43 fichiers obsoletes supprimes (scripts diag, docs internes, images doublon, artefacts deploiement)

--- remove-obsolete.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Remove obsolete files
-if exist public\build-redirect.htaccess (
-    del public\build-redirect.htaccess
-    echo Removed: public\build-redirect.htaccess
-) else (
-    echo File not found: public\build-redirect.htaccess
-)
-echo Done
