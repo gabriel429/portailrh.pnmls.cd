5add63acc91e66449ec651136d2c79b1ccbd1929
gabriel429 <igabrielmuya@gmail.com>
2026-05-12 00:17:06 +0100

    Serve Vite assets from public build path

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -22,8 +22,11 @@
     <meta name="application-name" content="PNMLS RH">
     <meta name="description" content="Portail Ressources Humaines - Programme National Multisectoriel de Lutte contre le Sida">
 
-    @vite('resources/js/app.js')
     @php
+        $manifest = [];
+        $viteEntry = null;
+        $viteImports = [];
+        $viteAsset = fn (string $path) => asset('public/build/' . ltrim($path, '/'));
         $dashboardCssFiles = [];
         $manifestPath = public_path('build/manifest.json');
         $dashboardManifestEntries = [
@@ -32,9 +35,19 @@
         ];
 
         if (! app()->isLocal() && is_file($manifestPath)) {
-            $manifest = json_decode((string) file_get_contents($manifestPath), true);
+            $decodedManifest = json_decode((string) file_get_contents($manifestPath), true);
+            $manifest = is_array($decodedManifest) ? $decodedManifest : [];
+            $viteEntry = $manifest['resources/js/app.js'] ?? null;
 
-            if (is_array($manifest)) {
+            if (is_array($viteEntry)) {
+                foreach (($viteEntry['imports'] ?? []) as $importKey) {
+                    if (isset($manifest[$importKey]['file'])) {
+                        $viteImports[] = $manifest[$importKey]['file'];
+                    }
+                }
+            }
+
+            if ($manifest !== []) {
                 foreach ($dashboardManifestEntries as $entry) {
                     foreach (($manifest[$entry]['css'] ?? []) as $cssFile) {
                         $dashboardCssPath = public_path('build/' . ltrim($cssFile, '/'));
@@ -46,8 +59,23 @@
             }
         }
     @endphp
+    @if (app()->isLocal() || ! is_array($viteEntry) || empty($viteEntry['file']))
+    @vite('resources/js/app.js')
+    @else
+    @foreach (($viteEntry['css'] ?? []) as $cssFile)
+    <link rel="preload" as="style" href="{{ $viteAsset($cssFile) }}" />
+    @endforeach
+    <link rel="modulepreload" as="script" href="{{ $viteAsset($viteEntry['file']) }}" />
+    @foreach ($viteImports as $importFile)
+    <link rel="modulepreload" as="script" href="{{ $viteAsset($importFile) }}" />
+    @endforeach
+    @foreach (($viteEntry['css'] ?? []) as $cssFile)
+    <link rel="stylesheet" href="{{ $viteAsset($cssFile) }}" />
+    @endforeach
+    <script type="module" src="{{ $viteAsset($viteEntry['file']) }}"></script>
+    @endif
     @foreach ($dashboardCssFiles as $dashboardCssFile => $dashboardCssVersion)
-    <link rel="stylesheet" href="{{ asset('build/' . ltrim($dashboardCssFile, '/')) }}?v={{ $dashboardCssVersion }}">
+    <link rel="stylesheet" href="{{ $viteAsset($dashboardCssFile) }}?v={{ $dashboardCssVersion }}">
     @endforeach
     <link rel="stylesheet" href="{{ asset('css/rh-modern.css') }}">
 </head>
